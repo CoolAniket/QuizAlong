2e56c6d0b9a1f2cedf890977e39ee66b30d45bdf
ibrahimgharyali <ibrahimgharyali@gmail.com>
2022-04-06 18:27:24 +0530

    WIP razor pay

--- Documents/Android/QuizAlong/app/src/main/java/com/thequizapp/quizalong/view/payment/PaymentActivity.java
+++ Documents/Android/QuizAlong/app/src/main/java/com/thequizapp/quizalong/view/payment/PaymentActivity.java
@@ -1,7 +1,9 @@
 package com.thequizapp.quizalong.view.payment;
 
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.razorpay.Checkout;
 import com.thequizapp.quizalong.R;
@@ -10,32 +12,63 @@ import com.thequizapp.quizalong.view.BaseActivity;
 
 import org.json.JSONObject;
 
+import androidx.databinding.DataBindingUtil;
+
 public class PaymentActivity extends BaseActivity {
     ActivityPaymentBinding binding;
 //    PaymentViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_payment);
+        initListener();
+        /*
+         To ensure faster loading of the Checkout form,
+          call this method as early as possible in your checkout flow.
+         */
+        Checkout.preload(getApplicationContext());
+
         Checkout checkout = new Checkout();
         checkout.setKeyID("rzp_test_RDLFlV1Kg8XmCo");
+    }
 
-        initiatePyment();
+    private void initListener() {
+        binding.startPayment.setOnClickListener((view) -> {
+            initiatePyment();
+        });
     }
 
     private void initiatePyment() {
-        /*try {
-            RazorpayClient razorpay = new RazorpayClient("[YOUR_KEY_ID]", "[YOUR_KEY_SECRET]");
+        /*
+          You need to pass current activity in order to let Razorpay create CheckoutActivity
+         */
+        final Activity activity = this;
 
-            JSONObject orderRequest = new JSONObject();
-            orderRequest.put("amount", 1); // amount in the smallest currency unit
-            orderRequest.put("currency", "INR");
-            orderRequest.put("receipt", "order_rcptid_11");
+        final Checkout co = new Checkout();
 
-            Order order = razorpay.Orders.create(orderRequest);
-        } catch (RazorpayException e) {
-            // Handle Exception
-            System.out.println(e.getMessage());
-        }*/
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "Razorpay Corp");
+            options.put("description", "Demoing Charges");
+            options.put("send_sms_hash",true);
+            options.put("allow_rotation", true);
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            options.put("currency", "INR");
+            options.put("amount", "1");
+
+            JSONObject preFill = new JSONObject();
+            preFill.put("email", "test@razorpay.com");
+            preFill.put("contact", "9876543210");
+
+            options.put("prefill", preFill);
+
+            co.open(activity, options);
+        } catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                    .show();
+            e.printStackTrace();
+        }
     }
 }
